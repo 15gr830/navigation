2990d837b58aa89daf8415d24808950ff974428b
Jens <nens90@gmail.com>
2015-05-10 17:32:38 +0200

    ..

--- external/startup_ros.sh
+++ external/startup_ros.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#source /opt/ros/indigo/setup.bash
-#ource /home/odroid/ros/devel/setup.bash
-#export ROS_IP=$(hostname -I)
-#export ROS_MASTER_URI=http://$ROS_IP:11311
+source /opt/ros/indigo/setup.bash
+source /home/odroid/ros/devel/setup.bash
+export ROS_IP=$(hostname -I)
+export ROS_MASTER_URI=http://$ROS_IP:11311
 
 # Initialise ROS and nodes
 roscore &
