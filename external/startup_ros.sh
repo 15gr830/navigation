9212c789d5ccc3b5601d0d2892abd6549dc78a41
Jens <nens90@gmail.com>
2015-05-10 17:31:27 +0200

    ..

--- external/startup_ros.sh
+++ external/startup_ros.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
-### BEGIN INIT INFO
-# Provides:          Startup ROS
-# Required-Start:    $remote_fs $syslog
-# Required-Stop:     $remote_fs $syslog
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Short-Description: Start daemon at boot time
-# Description:       Enable service provided by daemon.
-### END INIT INFO
-
-source /opt/ros/indigo/setup.bash
-source /home/odroid/ros/devel/setup.bash
-export ROS_IP=$(hostname -I)
-export ROS_MASTER_URI=http://$ROS_IP:11311
+#source /opt/ros/indigo/setup.bash
+#ource /home/odroid/ros/devel/setup.bash
+#export ROS_IP=$(hostname -I)
+#export ROS_MASTER_URI=http://$ROS_IP:11311
 
 # Initialise ROS and nodes
 roscore &
