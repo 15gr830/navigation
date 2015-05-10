49f8272bf428a03f7ba6dc73dd3ccf00f7fedfec
Jens <nens90@gmail.com>
2015-05-10 15:48:41 +0200

    ..

--- external/startup_ros.sh
+++ external/startup_ros.sh
@@ -13,12 +13,11 @@
 source /opt/ros/indigo/setup.bash
 source /home/odroid/ros/devel/setup.bash
 export ROS_IP=$(hostname -I)
-export ROS_MASTER_URI=http://192.168.1.149:11311
+export ROS_MASTER_URI=http://$ROS_IP:11311
 #export ROS_MASTER_URI=http://$ROS_IP:11311
 
 # Initialise ROS and nodes
 roscore &
-#roslaunch mavros px4_odrone.launch
-#roslaunch usb_cam usb_cam.launch
+roslauch navigation nav.launch &
 
 exit 0
