f2f2e585d073fba674e641ef985d8eb11566f6e8
Jens <nens90@gmail.com>
2015-05-11 09:27:57 +0200

    ..

--- external/startup_ros.sh
+++ external/startup_ros.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+
+while true
+do
+	ping -c 1 google.com
+
+	if [[ $? == 0 ]];
+		then
+		echo ‘Network available.’
+		break;
+	else
+		echo ‘Network is not available, waiting..’
+		sleep 5
+	fi
+	done
+
+# Initialise ROS and nodes
 source /opt/ros/indigo/setup.bash
-source /home/odroid/ros/devel/setup.bash
+#source /home/odroid/ros/devel/setup.bash
 export ROS_IP=$(hostname -I)
 export ROS_MASTER_URI=http://$ROS_IP:11311
 
-# Initialise ROS and nodes
-roscore &
-roslaunch navigation nav.launch &
+#roscore
+roslaunch navigation nav.launch
