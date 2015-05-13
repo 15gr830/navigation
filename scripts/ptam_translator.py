6d08bce095e4e6ce0fcff4b33c024ade339062c6
Jens <nens90@gmail.com>
2015-05-13 11:53:06 +0200

    ..

--- scripts/ptam_translator.py
+++ scripts/ptam_translator.py
@@ -4,12 +4,17 @@ import rospy
 import from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped
 
 def callback(data):
-	pos = PoseStamped()
+	ptam_pos = PoseStamped()
+	ptam_pos.header = data.header
+	ptam_pos.pose = data.pose.pose
+
+	pub_ptam.publish(ptam_pos)
 
 def get_ptam_data():
+	global pub_ptam
 	rospy.init_node('ptam_translator', anonymous=False)
 	rospy.Subscriber("/vslam/pose",PoseWithCovarianceStamped, callback)
-	pub = rospy.Publisher('/mavros/vision')
+	pub_ptam = rospy.Publisher('/mavros/vision_pose/pose', PoseStamped, queue_size=10)
 
 	rospy.spin()
 
