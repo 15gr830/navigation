39999ab269d62af8664a984b05cc24a3e57d3e5b
Jens <nens90@gmail.com>
2015-05-15 16:57:55 +0200

    ..

--- src/ptam_translator.py
+++ src/ptam_translator.py
@@ -2,7 +2,7 @@
 
 import rospy
 from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped
-from sensor_msgs.msg import Imu
+# from sensor_msgs.msg import Imu
 
 def write_to_pixhawk(data):
 	ptam_pos = PoseStamped()
@@ -11,18 +11,19 @@ def write_to_pixhawk(data):
 
 	pub_ptam.publish(ptam_pos)
 
-def tf_imu_data(data):
-	imu_data = Imu()
-	imu_data = data
-	pub_imu.publish(imu_data)
+# def tf_imu_data(data):
+# 	imu_data = Imu()
+# 	imu_data = data
+# 	pub_imu.publish(imu_data)
 
 def get_ptam_data():
-	global pub_ptam, pub_imu
+	global pub_ptam 
+	# global pub_imu
 	rospy.init_node('ptam_translator', anonymous=False)
 	rospy.Subscriber("/vslam/pose",PoseWithCovarianceStamped, write_to_pixhawk)
-	rospy.Subscriber("/mavros/imu/data", Imu, tf_imu_data)
+	# rospy.Subscriber("/mavros/imu/data", Imu, tf_imu_data)
 	pub_ptam = rospy.Publisher('/mavros/vision_pose/pose', PoseStamped, queue_size=10)
-	pub_imu = rospy.Publisher('/vslam/imu', Imu, queue_size=10)
+	# pub_imu = rospy.Publisher('/vslam/imu', Imu, queue_size=10)
 
 	rospy.spin()
 
