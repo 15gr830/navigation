18eb2be123c2e9b30fc9ce79b460b1be3e69b2f0
Jens <nens90@gmail.com>
2015-05-17 11:18:07 +0200

    ..

--- src/safety_node.py
+++ src/safety_node.py
@@ -2,13 +2,82 @@
 from __future__ import print_function
 
 import rospy
+import thread
+import threading
+import time
 import mavros
 import struct
 import numpy as np
 from mavros.utils import *
 from mavros import command
 from geometry_msgs.msg import PoseStamped
+from std_msgs.msg import Header
+from std_msgs.msg import String
 import parameter as parm
+
+# class Setpoint:
+ 
+#     def __init__(self, pub, rospy):
+#         self.pub = pub
+#         self.rospy = rospy
+ 
+#         self.x = 0.0
+#         self.y = 0.0
+#         self.z = 0.0
+ 
+#         try:
+#             thread.start_new_thread( self.navigate, () )
+#         except:
+#             print("Error: Unable to start thread")
+ 
+#         # TODO(simon): Clean this up.
+#         self.done = False
+#         self.done_evt = threading.Event()
+#         sub = rospy.Subscriber('/mavros/mocap/pose', PoseStamped, self.reached)
+ 
+#     def navigate(self):
+#         rate = self.rospy.Rate(10) # 10hz
+ 
+#         msg = PoseStamped()
+#         msg.header = Header() 
+#         msg.header.frame_id = "games_on_tracks"
+#         msg.header.stamp = rospy.Time.now()
+ 
+#         while 1:
+#             msg.pose.position.x = self.x
+#             msg.pose.position.y = self.y
+#             msg.pose.position.z = self.z
+ 
+#             # For demo purposes we will lock yaw/heading to north.
+#             yaw_degrees = 0  # North
+#             yaw = radians(yaw_degrees)
+#             quaternion = quaternion_from_euler(0, 0, yaw)
+#             msg.pose.orientation = Quaternion(*quaternion)
+ 
+#             self.pub.publish(msg)
+ 
+#             rate.sleep()
+ 
+#     def set(self, x, y, z, delay=0, wait=True):
+#         self.done = False
+#         self.x = x
+#         self.y = y
+#         self.z = z
+ 
+#         if wait:
+#             rate = rospy.Rate(5)
+#             while not self.done:
+#                 rate.sleep()
+ 
+#         time.sleep(delay)
+ 
+ 
+#     def reached(self, topic):
+#             #print topic.pose.position.z, self.z, abs(topic.pose.position.z - self.z)
+#             if abs(topic.pose.position.x - self.x) < 0.5 and abs(topic.pose.position.y - self.y) < 0.5 and abs(topic.pose.position.z - self.z) < 0.5:
+#                 self.done = True
+ 
+#             self.done_evt.set()
 
 def arm(state):
     try:
@@ -56,8 +125,8 @@ def safety_area(data):
 
 
 def odrone_interface():
-    rospy.init_node('odrone_interface', anonymous=False)
     rospy.Subscriber('/vicon_data', PoseStamped, safety_area)
+    rospy.init_node('odrone_interface', anonymous=False)
 
     
     while True:
