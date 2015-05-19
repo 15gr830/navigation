615fa991e531733251cf6d1338a91f76bb2c885c
Jens <nens90@gmail.com>
2015-05-19 21:41:39 +0200

    ..

---
+++ src/mat2ros2.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+import rospy, SocketServer, thread, threading, struct
+from std_msgs.msg import String
+from geometry_msgs.msg import PoseStamped
+
+class Positions:
+    def __init__(self, pub_got, pub_vicon, rospy):
+        self.pub_got = pub_got
+        self.pub_vicon = pub_vicon
+        self.rospy = rospy
+        
+        self.HOST = "0.0.0.0"
+        self.PORT = 3232
+
+        try:
+            thread.start_new_thread( self.get_data, () )
+        except:
+            print("Error: Unable to start thread")
+
+    def get_data(self) :
+        server = SocketServer.UDPServer((self.HOST, self.PORT), MatlabUDPHandler(self.pub_got, self.pub_vicon, self.rospy))
+        server.serve_forever()
+
+class MatlabUDPHandler(SocketServer.BaseRequestHandler):
+    def __init__(self, pub_got, pub_vicon, rospy):
+        self.pub_got = pub_got
+        self.pub_vicon = pub_vicon
+        self.rospy = rospy
+
+    def handle(self):
+        data = self.request[0]
+        socket = self.request[0]
+        # print "%s wrote:" % self.client_address[0]
+        numOfValues = len(data) / 8
+        data = struct.unpack('>' + 'd' * numOfValues, data)
+
+        now = rospy.get_rostime()
+        got_pos = PoseStamped()
+        vicon_pos = PoseStamped()
+        #rospy.loginfo("Time %i", now.secs)
+        got_pos.header.stamp.secs = now.secs
+        got_pos.header.stamp.nsecs = now.nsecs
+        got_pos.pose.position.x = data[0]
+        got_pos.pose.position.y = data[1]
+        got_pos.pose.position.z = data[2]
+
+        vicon_pos.header.stamp.secs = now.secs
+        vicon_pos.header.stamp.nsecs = now.nsecs
+        vicon_pos.pose.position.x = data[3]
+        vicon_pos.pose.position.y = data[4]
+        vicon_pos.pose.position.z = data[5]
+        vicon_pos.pose.orientation.x = data[6]
+        vicon_pos.pose.orientation.y = data[7]
+        vicon_pos.pose.orientation.z = data[8]
+        vicon_pos.pose.orientation.w = data[9]
+
+        self.pub_got.publish(got_pos)
+        self.pub_vicon.publish(vicon_pos)
+
+
+def init():
+    pub_got = rospy.Publisher('/mavros/mocap/pose', PoseStamped, queue_size=1)
+    pub_vicon = rospy.Publisher('/vicon_data', PoseStamped, queue_size=1)
+    rospy.init_node('mat2ros', anonymous=False)
+
+    Positions(pub_got,pub_vicon,rospy)
+
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        init()
+    except rospy.ROSInterruptException:
+        pass
