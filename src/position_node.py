ae67136df04e63374ab8a2c811bac308377832d7
Jens <nens90@gmail.com>
2015-05-23 17:43:53 +0200

    ..

--- src/position_node.py
+++ src/position_node.py
@@ -45,15 +45,15 @@ class MatlabUDPHandler(SocketServer.BaseRequestHandler):
         got_pos.header.stamp.secs = now.secs
         got_pos.header.stamp.nsecs = now.nsecs
         
-        # GOT Data
-        got_pos.pose.position.x = data[0]/1000
-        got_pos.pose.position.y = data[1]/1000
-        got_pos.pose.position.z = data[2]/1000
+        # # GOT Data
+        # got_pos.pose.position.x = data[0]/1000
+        # got_pos.pose.position.y = data[1]/1000
+        # got_pos.pose.position.z = data[2]/1000
 
-        # # Switched to Vicon data 
-        # got_pos.pose.position.x = data[3]/1000
-        # got_pos.pose.position.y = data[4]/1000
-        # got_pos.pose.position.z = data[5]/1000
+        # Switched to Vicon data 
+        got_pos.pose.position.x = data[3]/1000
+        got_pos.pose.position.y = data[4]/1000
+        got_pos.pose.position.z = data[5]/1000
 
         vicon_pos.header.stamp.secs = now.secs
         vicon_pos.header.stamp.nsecs = now.nsecs
